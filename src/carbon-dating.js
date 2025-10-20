4763476333e5ce4dd2428091d3bc9b5dc71f1840
JackalTabaqui <sob.selena@gmail.com>
2025-10-21 00:20:32 +0300

    feat: solve carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY)
+    return false;
+
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  const t = Math.log(MODERN_ACTIVITY / activity) / k;
+  return Math.ceil(t);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
